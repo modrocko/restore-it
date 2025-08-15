21bcac5feb06c7e5b0f0066cb17fe49850c7840b
Todd Clarke <77644845+modrocko@users.noreply.github.com>
2025-08-15 11:03:28 -0700

    Restore previous safari code
    I don't like the last fix, seems like a hack so I reverted to prev code.

--- scripts/restore_session.py
+++ scripts/restore_session.py
@@ -18,43 +18,21 @@ def open_window_in_browser(bounds, urls, browser):
 
     if browser.lower() == "safari":
         script = f'''
-        set wasRunning to false
-        tell application "System Events"
-            set wasRunning to (name of processes) contains "Safari"
-        end tell
-
         tell application "Safari"
             activate
-            if not wasRunning then
-                launch
-                delay 0.4
-                -- close any default blank windows Safari opened
-                repeat with w in (every window)
-                    try
-                        if (count of tabs of w) = 0 then
-                            close w
-                        else if (count of tabs of w) = 1 then
-                            set u to URL of tab 1 of w
-                            if u is missing value or u is "" or u is "about:blank" then close w
-                        end if
-                    end try
-                end repeat
-            end if
-
-            -- now make your saved session
-            make new document with properties {{URL: "{urls[0]}"}}
+            set newDoc to make new document
             set bounds of front window to {{{bounds["x"]}, {bounds["y"]}, {bounds["x"] + bounds["width"]}, {bounds["y"] + bounds["height"]}}}
+            set URL of front document to "{urls[0]}"
         '''
 
         if len(urls) > 1:
             script += '\ntell front window'
             for url in urls[1:]:
-                script += '\nmake new tab at end of tabs'
-                script += f'\nset URL of last tab to "{url}"'
+                script += f'\nset newTab to make new tab at end of tabs'
+                script += f'\nset URL of newTab to "{url}"'
             script += '\nend tell'
 
         script += '\nend tell'
-
 
     else:
         first_url = json.dumps(urls[0])
